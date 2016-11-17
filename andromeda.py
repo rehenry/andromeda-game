7e9910021ad257a7f1dc21ce80e92c80c4696523
rehenry <rebeccahenry2014@gmail.com>
2016-11-16 23:26:36 -0500

    Updated with WIP images

--- andromeda.py
+++ andromeda.py
@@ -1,14 +1,30 @@
 import pygame
 
+X_MAX = 800
+Y_MAX = 600
 
 class Spaceship(pygame.sprite.Sprite):
     def __init__(self):
         super(Spaceship, self).__init__()
-        self.image = pygame.image.load("images/dirt.jpg").convert()
-        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.image = pygame.image.load("images/spaceship.png").convert_alpha()
+        self.size = self.image.get_size()
+        self.image = pygame.transform.smoothscale(self.image, (int(self.size[0]/4), int(self.size[1]/4)))
+        #self.image = pygame.transform.smoothscale(self.image, (50, 50))
         self.rect = self.image.get_rect() 
+        self.rect.center = (X_MAX/2, Y_MAX/2)
 
+class Asteroid(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Asteroid, self).__init__()
+        self.image = pygame.image.load("images/asteroid_lg_1.png").convert_alpha()
+        self.size = self.image.get_size()
+        self.image = pygame.transform.smoothscale(self.image, (int(self.size[0]/5), int(self.size[1]/5)))
+        #self.image = pygame.transform.smoothscale(self.image, (50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.center = (X_MAX/2, Y_MAX - 400)
+        
 def main():
+ 
     pygame.init() 
     screen = pygame.display.set_mode((800,600))
     pygame.display.set_caption("Andromeda")  
@@ -17,16 +33,17 @@ def main():
     background = pygame.transform.scale(background, (800, 600))
     screen.blit(background, (0,0))
 
-    my_Sprite = Spaceship()
-    my_group = pygame.sprite.Group(my_Sprite)
+    ship = Spaceship()
+    asteroid = Asteroid()
+    obj_group = pygame.sprite.Group(ship, asteroid)
 
     while True:
         event = pygame.event.poll()    
         if event.type == pygame.QUIT:  
-            break                   
+            break 
 
-        my_group.update()
-        my_group.draw(screen)
+        obj_group.update()
+        obj_group.draw(screen)
         pygame.display.flip()
 
     pygame.quit()     
